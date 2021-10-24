437a8e1a59fad09673ba96e96d02fbc8c1276b53
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-24 21:42:07 +0200

    new post

---
+++ src/components/PostNew.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <form action="post">
+      <div class="form-control">
+        <label for="post">Write a new post...</label>
+        <textarea name="post" rows="4" cols="25" placeholder="Say something..." />
+      </div>
+      <AppButton size="small">
+        Publish
+        <font-awesome-icon icon="fa-solid fa-paper-plane" class="icon" />
+      </AppButton>
+    </form>
+  </div>
+</template>
+
+<script>
+import AppButton from "@/components/AppButton.vue";
+
+export default {
+  components: {
+    AppButton,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 20px;
+  margin: 15px;
+  border: 2px solid $color-fade-lighten;
+}
+.form-control {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: block;
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  textarea {
+    margin: 10px 0;
+    padding: 3px 10px;
+    background-color: lighten($color-fade-lighten, 5%);
+    border: none;
+    border-radius: 5px;
+
+    &:focus {
+      border: 2px solid $color-primary;
+      background-color: white;
+    }
+
+    &:focus + label {
+      color: $color-primary-darken;
+    }
+
+    &:focus-visible {
+      outline: none;
+    }
+  }
+}
+.icon {
+  margin-left: 1em;
+}
+
+h1 {
+  font-size: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="div">
     <Header />
-    <PostCreate></PostCreate>
+    <PostNew></PostNew>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
-import PostCreate from "../components/PostCreate.vue";
+import PostNew from "../components/PostNew.vue";
 
 export default {
   components: {
     Header,
-    PostCreate,
+    PostNew,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    this.posts = [
+      //Should use API to fetch data
+      {
+        id: 1,
+        text: "Hello World. This is a post from the first user.",
+        day: "October 24th at 21:14",
+        likes: 5,
+      },
+      {
+        id: 2,
+        text: "This is another post from another user. Yes!",
+        day: "October 24th at 23:50",
+        likes: 0,
+      },
+    ];
   },
 };
 </script>
