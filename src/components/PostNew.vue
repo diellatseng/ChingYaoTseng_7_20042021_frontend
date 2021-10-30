d134d9c86395d18e1484bfab5c788a7bec3e7dce
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-30 11:24:17 +0200

    create new post function

--- src/components/PostNew.vue
+++ src/components/PostNew.vue
@@ -2,8 +2,13 @@
   <div class="container">
     <form @submit.prevent>
       <div class="form-control">
-        <label for="post"><slot></slot></label>
+        <label for="post"><slot name="title">Write a new post...</slot></label>
         <textarea v-model="dataPost.content" name="post" rows="4" cols="25" placeholder="Say something..." required/>
+      </div>
+
+      <div class="form-control">
+        <label for="post__image"><slot name="title__image"></slot>Upload an image...</label>
+        <input type="file" accept="image/*" @change="uploadImage($event)" id="post__image">
       </div>
       <button type="submit" class="button small" @click="createPost">
         Publish
@@ -22,29 +27,57 @@ export default {
   data() {
     return {
       dataPost: {
-        // author_id: localStorage.author_id
-        author_id: 1,
-        content: null, 
-        // img: null
+        content: '',
+        author_id: ''
       },
       dataPostString: "",
     }
   },
+  
+
   methods: {
     createPost() {
-      this.dataPostString = JSON.stringify(this.dataPost);
-      console.log(this.dataPostString)
+      // this.dataPostString = JSON.stringify(this.dataPost);
+      if (localStorage.userId) {
+        this.dataPost.author_id = localStorage.userId;
+        console.log(this.dataPost.author_id);
+        console.log(this.dataPost)
+      }
 
-      axios.post("http://localhost:3000/api/post", this.dataPostString)
+      axios.post("http://localhost:3000/api/post", this.dataPost)
         .then(response => {
           let res = JSON.parse(response.data);
-            // alert("Your post has been created!");                               //should refresh here
+            alert("Your post has been created!");
+            this.dataPost.content = "";
           console.log(res)
         })
          .catch(function (error) {
           console.log(error.toJSON());
       })
+    },
 
+    uploadImage(event) {
+      const URL = 'http://localhost:3000/api/post'; 
+
+      let data = new FormData();
+      data.append('name', 'my-picture');
+      data.append('file', event.target.files[0]); 
+
+      let config = {
+        header : {
+          'Content-Type' : 'image/png'
+        }
+      }
+
+      axios.put(
+        URL, 
+        data,
+        config
+      ).then(
+        response => {
+          console.log('image upload response > ', response)
+        }
+      )
     }
   }
 };
