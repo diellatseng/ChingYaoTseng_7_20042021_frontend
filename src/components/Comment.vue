66646c69740c92a78f180a9ccb6abf46f0aef7e2
C.Y. Tseng <diella.tseng@gmail.com>
2021-10-31 15:32:36 +0100

    get comments

---
+++ src/components/Comment.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <p class="post__date">{{ comment.created_at }}</p>
+        <p class="post__date">{{ comment.content }}</p>
+        <p class="post__date">{{ comment.author.full_name }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Comment",
+    props: {
+        comment: Object,
+  },
+}
+</script>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -9,25 +9,63 @@
         <font-awesome-icon icon="fa-regular fa-heart" /> 
         {{ post._count.likes }}
       </div>
+
       <div v-if="post._count.comments > 0" class="actions__comment">{{ post._count.comments }}
         <span v-if="post._count.comments == 1">comment</span>
         <span v-else>comments</span>
       </div>
+
+      <div class="container">
+        <div :key="comment.id" v-for="comment in post.comments" >
+          <div class="container">
+              <h2 class="">{{ comment }}</h2>
+              <p class="">{{ comment.created_at }}</p>
+              <p class="">{{ comment.content }}</p>
+          </div>
+        </div>
+      </div>
+   
     </div>
+        
   </div>
 </template>
 
 <script>
+
+// import Comment from "./Comment.vue";
+// import axios from "axios";
+
+
 export default {
   name: "Post",
+  // components: {
+  //   Comment
+  // },
   props: {
     post: Object,
   },
+  
   data(){
     return {
-      seen: true
+      seen: true,
+      // comments: []
     }
-  }
+  },
+  
+  // methods: {
+  //   created() {
+  //     axios
+  //       .get('http://localhost:3000/api/post/comment', {headers: {Authorization: 'Bearer ' + localStorage.token}})
+  //       .then(response => {
+  //         this.comments = response.data
+  //         console.log(this.comments)
+  //       })
+  //       .catch (error => {
+  //         alert('You are not logged in!')
+  //         console.log(error.response)
+  //       })
+  //   },
+  // }
 };
 </script>
 
